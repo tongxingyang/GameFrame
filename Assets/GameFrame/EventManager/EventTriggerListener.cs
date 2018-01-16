5790ba3e2ea4f2c03c04ac46b5d4b5d8c0a48249
tongxingyang <ww386362087@gmail.com>
2018-01-16 17:34:20 +0800

    提交eventlisten and miniJson

---
+++ Assets/GameFrame/EventManager/EventTriggerListener.cs
@@ -0,0 +1,335 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UIFrameWork
+{
+	public delegate void OnTouchEventHandle(GameObject _sender, object _args, params object[]_objects);
+	public enum EnumTouchEventType
+	{
+		OnClick,
+		OnDoubleClick,
+		OnDown,
+		OnUp,
+		OnEnter,
+		OnExit,
+		OnSelect,  
+		OnUpdateSelect,  
+		OnDeSelect, 
+		OnDrag, 
+		OnDragEnd,
+		OnDrop,
+		OnScroll, 
+		OnMove,
+	}
+    public class TouchHandle
+    {
+        private event OnTouchEventHandle eventHandle = null;
+        /// <summary>
+        /// 点击附加的额外参数
+        /// </summary>
+        private object[] handleParams;
+        public TouchHandle()
+        {
+            DestoryHandle();
+        }
+        /// <summary>
+        /// 删除所有的委托
+        /// </summary>
+        public void DestoryHandle()
+        {
+            if (null != eventHandle)
+            {
+                eventHandle -= eventHandle;
+                eventHandle = null;
+            }
+        }
+
+        public void SetHandle(OnTouchEventHandle handle, params object[] _params)
+        {
+            eventHandle += handle;
+            handleParams = _params;
+        }
+
+        public void CallEventHandle(GameObject go,object args)
+        {
+            if (null != eventHandle)
+            {
+                eventHandle(go, args, handleParams);
+            }
+        }
+    }
+    public class EventTriggerListener : MonoBehaviour,
+    IPointerClickHandler,
+    IPointerDownHandler,  
+    IPointerUpHandler, 
+    IPointerEnterHandler,  
+    IPointerExitHandler,  
+ 
+    ISelectHandler,  
+    IUpdateSelectedHandler,  
+    IDeselectHandler, 
+
+    IDragHandler,  
+    IEndDragHandler,  
+    IDropHandler,  
+    IScrollHandler,  
+    IMoveHandler  
+    {
+        //定义相关点击事件
+        public TouchHandle onClick;  
+        public TouchHandle onDoubleClick; 
+        public TouchHandle onDown;  
+        public TouchHandle onEnter;  
+        public TouchHandle onExit;  
+        public TouchHandle onUp;  
+        public TouchHandle onSelect;  
+        public TouchHandle onUpdateSelect;  
+        public TouchHandle onDeSelect;  
+        public TouchHandle onDrag;  
+        public TouchHandle onDragEnd;  
+        public TouchHandle onDrop;  
+        public TouchHandle onScroll;  
+        public TouchHandle onMove;
+
+        public static EventTriggerListener Get(GameObject go)
+        {
+            EventTriggerListener listener = go.GetComponent<EventTriggerListener>();
+            if (listener == null)
+            {
+                listener = go.AddComponent<EventTriggerListener>();
+            }
+            return listener;
+        }
+
+        void OnDestroy()
+        {
+            RemoveHandle (onClick);
+            RemoveHandle (onDoubleClick);
+            RemoveHandle (onDown);
+            RemoveHandle (onEnter);
+            RemoveHandle (onExit);
+            RemoveHandle (onUp);
+            RemoveHandle (onDrop);
+            RemoveHandle (onDrag);
+            RemoveHandle (onDragEnd);
+            RemoveHandle (onScroll);
+            RemoveHandle (onMove);
+            RemoveHandle (onUpdateSelect);
+            RemoveHandle (onSelect);
+            RemoveHandle (onDeSelect);
+        }
+
+        private void RemoveHandle(TouchHandle handle)
+        {
+            if (null != handle)
+            {
+                handle.DestoryHandle();
+                handle = null;
+            }
+        }
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (onClick != null)
+            {
+                onClick.CallEventHandle(this.gameObject,eventData);
+            }
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (onDown != null)
+            {
+                onDown.CallEventHandle(this.gameObject,eventData);
+            }
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (onUp != null)
+            {
+                onUp.CallEventHandle(this.gameObject,eventData);
+            }
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (onEnter != null)
+            {
+                onEnter.CallEventHandle(this.gameObject,eventData);
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (onExit != null)
+            {
+                onExit.CallEventHandle(this.gameObject,eventData);
+            }
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            if (onSelect != null)
+            {
+                onSelect.CallEventHandle(this.gameObject,eventData);
+            }
+        }
+
+        public void OnUpdateSelected(BaseEventData eventData)
+        {
+            if (onUpdateSelect != null)
+            {
+                onUpdateSelect.CallEventHandle(this.gameObject,eventData);
+            }
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            if (onDeSelect != null)
+            {
+                onDeSelect.CallEventHandle(this.gameObject,eventData);
+            }
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (onDrag != null)
+            {
+                onDrag.CallEventHandle(this.gameObject,eventData);
+            }
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            if (onDragEnd != null)
+            {
+                onDragEnd.CallEventHandle(this.gameObject,eventData);
+            }
+        }
+
+        public void OnDrop(PointerEventData eventData)
+        {
+            if (onDrop != null)
+            {
+                onDrop.CallEventHandle(this.gameObject,eventData);
+            }
+        }
+
+        public void OnScroll(PointerEventData eventData)
+        {
+            if (onScroll != null)
+            {
+                onScroll.CallEventHandle(this.gameObject,eventData);
+            }
+        }
+
+        public void OnMove(AxisEventData eventData)
+        {
+            if (onMove != null)
+            {
+                onMove.CallEventHandle(this.gameObject,eventData);
+            }
+        }
+
+        public void SetEventHandle(EnumTouchEventType type,OnTouchEventHandle _handle,params object[] _params)
+        {
+            switch (type)
+            {
+                case EnumTouchEventType.OnClick:
+				if (null == onClick) {
+					onClick = new TouchHandle();
+				}
+				onClick.SetHandle(_handle, _params);
+				break;
+			case EnumTouchEventType.OnDoubleClick:
+				if (null == onDoubleClick) {
+					onDoubleClick = new TouchHandle();
+				}
+				onDoubleClick.SetHandle(_handle, _params);
+				break;
+			case EnumTouchEventType.OnDown:
+				if (onDown == null) {
+					onDown = new TouchHandle();
+				}
+				onDown.SetHandle (_handle, _params);
+				break;
+			case EnumTouchEventType.OnUp:
+				if (onUp == null) {
+					onUp = new TouchHandle();
+				}
+				onUp.SetHandle (_handle, _params);
+				break;
+			case EnumTouchEventType.OnEnter:
+				if (onEnter == null) {
+					onEnter = new TouchHandle();
+				}
+				onEnter.SetHandle (_handle, _params);
+				break;
+			case EnumTouchEventType.OnExit:
+				if (onExit == null) {
+					onExit = new TouchHandle();
+				}
+				onExit.SetHandle (_handle, _params);
+				break;
+			case EnumTouchEventType.OnDrag:
+				if (onDrag == null) {
+					onDrag = new TouchHandle();
+				}
+				onDrag.SetHandle (_handle, _params);
+				break;
+			case EnumTouchEventType.OnDrop:
+				if (onDrop == null) {
+					onDrop = new TouchHandle();
+				}
+				onDrop.SetHandle (_handle, _params);
+				break;
+
+			case EnumTouchEventType.OnDragEnd:
+				if (onDragEnd == null)
+				{
+					onDragEnd = new TouchHandle();
+				}
+				onDragEnd.SetHandle(_handle, _params);
+				break;
+			case EnumTouchEventType.OnSelect:
+				if (onSelect == null)
+				{
+					onSelect = new TouchHandle();
+				}
+				onSelect.SetHandle(_handle, _params);
+				break;
+			case EnumTouchEventType.OnUpdateSelect:
+				if (onUpdateSelect == null)
+				{
+					onUpdateSelect = new TouchHandle();
+				}
+				onUpdateSelect.SetHandle(_handle, _params);
+				break;
+			case EnumTouchEventType.OnDeSelect:
+				if (onDeSelect == null)
+				{
+					onDeSelect = new TouchHandle();
+				}
+				onDeSelect.SetHandle(_handle, _params);
+				break;
+			case EnumTouchEventType.OnScroll:
+				if (onScroll == null)
+				{
+					onScroll = new TouchHandle();
+				}
+				onScroll.SetHandle(_handle, _params);
+				break;
+			case EnumTouchEventType.OnMove:
+				if (onMove == null)
+				{
+					onMove = new TouchHandle();
+				}
+				onMove.SetHandle(_handle, _params);
+				break; 
+            }
+        }
+    }  
+}
+
---
+++ Assets/GameFrame/miniJSON/Json.cs
@@ -0,0 +1,561 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace com.tencent.pandora.MiniJSON
+{
+	public static class Json
+	{
+		private sealed class Parser : IDisposable
+		{
+			private enum TOKEN
+			{
+				NONE,
+				CURLY_OPEN,
+				CURLY_CLOSE,
+				SQUARED_OPEN,
+				SQUARED_CLOSE,
+				COLON,
+				COMMA,
+				STRING,
+				NUMBER,
+				TRUE,
+				FALSE,
+				NULL
+			}
+
+			private const string WORD_BREAK = "{}[],:\"";
+
+			private StringReader json;
+
+			private char PeekChar
+			{
+				get
+				{
+					return Convert.ToChar(this.json.Peek());
+				}
+			}
+
+			private char NextChar
+			{
+				get
+				{
+					return Convert.ToChar(this.json.Read());
+				}
+			}
+
+			private string NextWord
+			{
+				get
+				{
+					StringBuilder stringBuilder = new StringBuilder();
+					while (!Json.Parser.IsWordBreak(this.PeekChar))
+					{
+						stringBuilder.Append(this.NextChar);
+						if (this.json.Peek() == -1)
+						{
+							break;
+						}
+					}
+					return stringBuilder.ToString();
+				}
+			}
+
+			private Json.Parser.TOKEN NextToken
+			{
+				get
+				{
+					this.EatWhitespace();
+					if (this.json.Peek() == -1)
+					{
+						return Json.Parser.TOKEN.NONE;
+					}
+					char peekChar = this.PeekChar;
+					switch (peekChar)
+					{
+					case '"':
+						return Json.Parser.TOKEN.STRING;
+					case '#':
+					case '$':
+					case '%':
+					case '&':
+					case '\'':
+					case '(':
+					case ')':
+					case '*':
+					case '+':
+					case '.':
+					case '/':
+						Debug.Log("peekChar    "+peekChar);
+						break;
+					case ',':
+						this.json.Read();
+						return Json.Parser.TOKEN.COMMA;
+					case '-':
+					case '0':
+					case '1':
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '7':
+					case '8':
+					case '9':
+						return Json.Parser.TOKEN.NUMBER;
+					case ':':
+						return Json.Parser.TOKEN.COLON;
+					default:
+						switch (peekChar)
+						{
+						case '[':
+							return Json.Parser.TOKEN.SQUARED_OPEN;
+						case '\\':
+							break;
+						case ']':
+							this.json.Read();
+							return Json.Parser.TOKEN.SQUARED_CLOSE;
+						default:
+							switch (peekChar)
+							{
+							case '{':
+								return Json.Parser.TOKEN.CURLY_OPEN;
+							case '}':
+								this.json.Read();
+								return Json.Parser.TOKEN.CURLY_CLOSE;
+							}
+							break;
+						}
+						break;
+					}
+					string nextWord;
+					if ((nextWord = this.NextWord) != null)
+					{
+						if (nextWord == "false")
+						{
+							return Json.Parser.TOKEN.FALSE;
+						}
+						if (nextWord == "true")
+						{
+							return Json.Parser.TOKEN.TRUE;
+						}
+						if (nextWord == "null")
+						{
+							return Json.Parser.TOKEN.NULL;
+						}
+					}
+					return Json.Parser.TOKEN.NONE;
+				}
+			}
+
+
+
+			private Parser(string jsonString)
+			{
+				this.json = new StringReader(jsonString);
+			}
+
+			public static bool IsWordBreak(char c)
+			{
+				return char.IsWhiteSpace(c) || "{}[],:\"".IndexOf(c) != -1;
+			}
+			public static object Parse(string jsonString)
+			{
+				object result;
+				using (Json.Parser parser = new Json.Parser(jsonString))
+				{
+					result = parser.ParseValue();
+				}
+				return result;
+			}
+
+			public void Dispose()
+			{
+				this.json.Dispose();
+				this.json = null;
+			}
+
+			private Dictionary<string, object> ParseObject()
+			{
+				Dictionary<string, object> dictionary = new Dictionary<string, object>();
+				this.json.Read();
+				while (true)
+				{
+					Json.Parser.TOKEN nextToken = this.NextToken;
+					switch (nextToken)
+					{
+
+					case TOKEN.NONE:
+						return null;
+					case TOKEN.COMMA:
+						continue;
+					case TOKEN.CURLY_CLOSE:
+						return dictionary;
+					default:
+						string name = ParseString();
+						if (name == null)
+						{
+							return null;
+						}
+
+						if (NextToken != TOKEN.COLON)
+						{
+							return null;
+						}
+						// ditch the colon
+						this.json.Read();
+
+						// value
+						dictionary[name] = ParseValue();
+						break;
+					}
+				}
+
+			}
+
+			private List<object> ParseArray()
+			{
+				List<object> list = new List<object>();
+				this.json.Read();
+				bool flag = true;
+				while (flag)
+				{
+					Json.Parser.TOKEN nextToken = this.NextToken;
+					Json.Parser.TOKEN tOKEN = nextToken;
+					if (tOKEN == Json.Parser.TOKEN.NONE)
+					{
+						return null;
+					}
+					switch (tOKEN)
+					{
+					case Json.Parser.TOKEN.SQUARED_CLOSE:
+						flag = false;
+						continue;
+					case Json.Parser.TOKEN.COMMA:
+						continue;
+					}
+					object item = this.ParseByToken(nextToken);
+					list.Add(item);
+				}
+				return list;
+			}
+
+			private object ParseValue()
+			{
+				Json.Parser.TOKEN nextToken = this.NextToken;
+				return this.ParseByToken(nextToken);
+			}
+
+			private object ParseByToken(Json.Parser.TOKEN token)
+			{
+				switch (token)
+				{
+				case Json.Parser.TOKEN.CURLY_OPEN:
+					return this.ParseObject();
+				case Json.Parser.TOKEN.SQUARED_OPEN:
+					return this.ParseArray();
+				case Json.Parser.TOKEN.STRING:
+					return this.ParseString();
+				case Json.Parser.TOKEN.NUMBER:
+					return this.ParseNumber();
+				case Json.Parser.TOKEN.TRUE:
+					return true;
+				case Json.Parser.TOKEN.FALSE:
+					return false;
+				case Json.Parser.TOKEN.NULL:
+					return null;
+				}
+				return null;
+			}
+
+			private string ParseString()
+			{
+				StringBuilder stringBuilder = new StringBuilder();
+				this.json.Read();
+				bool flag = true;
+				while (flag)
+				{
+					if (this.json.Peek() == -1)
+					{
+						break;
+					}
+					char nextChar = this.NextChar;
+					char c = nextChar;
+					if (c == '"')
+					{
+						flag = false;
+					}
+					else if (c != '\\')
+					{
+						stringBuilder.Append(nextChar);
+					}
+					else if (this.json.Peek() == -1)
+					{
+						flag = false;
+					}
+					else
+					{
+						nextChar = this.NextChar;
+						char c2 = nextChar;
+						switch (c2)
+						{
+						case 'n':
+							stringBuilder.Append('\n');
+							continue;
+						case 'r':
+							stringBuilder.Append('\r');
+							continue;
+						case 't':
+							stringBuilder.Append('\t');
+							continue;
+						case 'u':
+							{
+								char[] array = new char[4];
+								for (int i = 0; i < 4; i++)
+								{
+									array[i] = this.NextChar;
+								}
+								stringBuilder.Append((char)Convert.ToInt32(new string(array), 16));
+								continue;
+							}
+						}
+						if (c2 != '"' && c2 != '/' && c2 != '\\')
+						{
+							if (c2 == 'b')
+							{
+								stringBuilder.Append('\b');
+							}
+							else if (c2 == 'f')
+							{
+								stringBuilder.Append('\f');
+							}
+						}
+						else
+						{
+							stringBuilder.Append(nextChar);
+						}
+					}
+				}
+				return stringBuilder.ToString();
+			}
+
+
+			private object ParseNumber()
+			{
+				
+				string number = NextWord;
+
+				if (number.IndexOf('.') == -1)
+				{
+					#if JSON_INT_64
+					long parsedInt;
+					Int64.TryParse(number, out parsedInt);
+					#else
+					int parsedInt;
+					Int32.TryParse(number, out parsedInt);
+					#endif
+					return parsedInt;
+				}
+
+				#if JSON_FLOAT_SINGLE
+				float parsedDouble;
+				Single.TryParse(number, out parsedDouble);
+				#else
+				double parsedDouble;
+				Double.TryParse(number, out parsedDouble);
+				#endif
+				return parsedDouble;
+
+			}
+
+			private void EatWhitespace()
+			{
+				while (char.IsWhiteSpace(this.PeekChar))
+				{
+					this.json.Read();
+					if (this.json.Peek() == -1)
+					{
+						return;
+					}
+				}
+			}
+		}
+
+		private sealed class Serializer
+		{
+			private StringBuilder builder;
+
+			private Serializer()
+			{
+				this.builder = new StringBuilder();
+			}
+
+			public static string Serialize(object obj)
+			{
+				Json.Serializer serializer = new Json.Serializer();
+				serializer.SerializeValue(obj);
+				return serializer.builder.ToString();
+			}
+
+			private void SerializeValue(object value)
+			{
+				if (value == null)
+				{
+					this.builder.Append("null");
+					return;
+				}
+				string str;
+				if ((str = (value as string)) != null)
+				{
+					this.SerializeString(str);
+					return;
+				}
+				if (value is bool)
+				{
+					this.builder.Append(((bool)value) ? "true" : "false");
+					return;
+				}
+				IList anArray;
+				if ((anArray = (value as IList)) != null)
+				{
+					this.SerializeArray(anArray);
+					return;
+				}
+				IDictionary obj;
+				if ((obj = (value as IDictionary)) != null)
+				{
+					this.SerializeObject(obj);
+					return;
+				}
+				if (value is char)
+				{
+					this.SerializeString(new string((char)value, 1));
+					return;
+				}
+				this.SerializeOther(value);
+			}
+
+			private void SerializeObject(IDictionary obj)
+			{
+				bool flag = true;
+				this.builder.Append('{');
+				foreach (object current in obj.Keys)
+				{
+					if (!flag)
+					{
+						this.builder.Append(',');
+					}
+					this.SerializeString(current.ToString());
+					this.builder.Append(':');
+					this.SerializeValue(obj[current]);
+					flag = false;
+				}
+				this.builder.Append('}');
+			}
+
+			private void SerializeArray(IList anArray)
+			{
+				this.builder.Append('[');
+				bool flag = true;
+				for (int i = 0; i < anArray.Count; i++)
+				{
+					object value = anArray[i];
+					if (!flag)
+					{
+						this.builder.Append(',');
+					}
+					this.SerializeValue(value);
+					flag = false;
+				}
+				this.builder.Append(']');
+			}
+
+			void SerializeString(string str)
+			{
+				builder.Append('\"');
+
+				char[] charArray = str.ToCharArray();
+				foreach (var c in charArray)
+				{
+					switch (c)
+					{
+					case '"':
+						builder.Append("\\\"");
+						break;
+					case '\\':
+						builder.Append("\\\\");
+						break;
+					case '\b':
+						builder.Append("\\b");
+						break;
+					case '\f':
+						builder.Append("\\f");
+						break;
+					case '\n':
+						builder.Append("\\n");
+						break;
+					case '\r':
+						builder.Append("\\r");
+						break;
+					case '\t':
+						builder.Append("\\t");
+						break;
+					default:
+						int codepoint = Convert.ToInt32(c);
+						if ((codepoint >= 32) && (codepoint <= 126))
+						{
+							builder.Append(c);
+						}
+						else
+						{
+							builder.Append("\\u");
+							builder.Append(codepoint.ToString("x4"));
+						}
+						break;
+					}
+				}
+
+				builder.Append('\"');
+			}
+
+			private void SerializeOther(object value)
+			{
+				if (value is float)
+				{
+					this.builder.Append(((float)value).ToString("R", CultureInfo.InvariantCulture));
+					return;
+				}
+				if (value is int || value is uint || value is long || value is sbyte || value is byte || value is short || value is ushort || value is ulong)
+				{
+					this.builder.Append(value);
+					return;
+				}
+				if (value is double || value is decimal)
+				{
+					this.builder.Append(Convert.ToDouble(value).ToString("R", CultureInfo.InvariantCulture));
+					return;
+				}
+				this.SerializeString(value.ToString());
+			}
+		}
+
+		public static object Deserialize(string json)
+		{
+			if (json == null)
+			{
+				return null;
+			}
+			return Json.Parser.Parse(json);
+		}
+
+		public static string Serialize(object obj)
+		{
+			return Json.Serializer.Serialize(obj);
+		}
+	}
+}
+
