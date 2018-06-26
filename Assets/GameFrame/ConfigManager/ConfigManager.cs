c848c6b65e623f58769b90beb821d43436c9fd15
tongxingyang <ww386362087@gmail.com>
2018-06-26 10:42:27 +0800

    异步加载config逻辑

--- Assets/GameFrame/ConfigManager/ConfigManager.cs
+++ Assets/GameFrame/ConfigManager/ConfigManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using UnityEngine;
@@ -148,6 +150,124 @@ namespace GameFrame.ConfigManager
             UnityEngine.Debug.Log(buffer);
             return buffer;
         }
+        private Dictionary<string,Action<string>> CallBacks = new Dictionary<string, Action<string>>();
+        public void AsynLoadConfig(string fileName, bool isCacheBundle, Action<string> callBack)
+        {
+            if (ConfigConst.ConfigBundleMode)
+            {
+                if (CallBacks.ContainsKey(fileName))
+                {
+                    Action<string> action = CallBacks[fileName];
+                    action += callBack;
+                    return;
+                }
+                else
+                {
+                    CallBacks[fileName] = callBack;
+                    SingletonMono<GameFrameWork>.GetInstance().StartCoroutine(AsynReadFile(fileName, isCacheBundle));
+                }
+            }
+            else
+            {
+                UnityEngine.Debug.LogError("编辑器模式不支持异步夹杂config文件  ConfigBundleMode==false");
+            }
+        }
 
+        private IEnumerator AsynReadFile(string fileName,bool isCacheBundle)
+        {
+            AssetBundle zipFile = null;
+            string content = string.Empty;
+            byte[] buffer = null;
+            string zipName = null;
+
+            using (CString.Block())
+            {
+                
+                CString sb = CString.Alloc(256);
+                sb.Append("config");
+                int pos = fileName.LastIndexOf('/');
+
+                if (pos > 0)
+                {
+                    sb.Append("_");                    
+                    sb.Append(fileName, 0, pos).ToLower().Replace('/', '_');                                        
+                    fileName = fileName.Substring(pos + 1);
+                }
+                zipName = sb.ToString();
+                
+                configMap.TryGetValue(zipName, out zipFile);
+            }
+            var isHaveCache = false;
+            if (zipFile == null)
+            {
+                isHaveCache = false;
+                AssetBundleCreateRequest assetBundleRequest;
+                assetBundleRequest = AssetBundle.LoadFromFileAsync(ConfigConst.configResDir + "/" + zipName+Platform.AssetBundleExt);
+                yield return assetBundleRequest;
+                if (assetBundleRequest.isDone)
+                {
+                    zipFile = assetBundleRequest.assetBundle;
+                    if (isCacheBundle)
+                    {
+                        configMap[zipName] = zipFile;
+                    }
+                }
+            }
+            else
+            {
+                isHaveCache = true;
+            }
+            
+            if (zipFile != null)
+            {
+                TextAsset configCode = null;
+#if UNITY_5 || UNITY_2017 
+                AssetBundleRequest assetBundleRequest;
+                assetBundleRequest = zipFile.LoadAssetAsync<TextAsset>(fileName);
+                yield return assetBundleRequest;
+                if (assetBundleRequest.isDone)
+                {
+                    configCode = (TextAsset) assetBundleRequest.asset;
+                }
+#else
+                 configCode = zipFile.Load(fileName, typeof(TextAsset)) as TextAsset;
+#endif
+
+                if (configCode != null)
+                {
+                    buffer = configCode.bytes;
+                    Resources.UnloadAsset(configCode);
+                    byte[] decrypt = ConfigEncrypt.Decrypt(buffer);
+                    content = System.Text.Encoding.UTF8.GetString(decrypt);
+                }
+                if (!isCacheBundle && !isHaveCache)
+                {
+                    zipFile.Unload(true);
+                }
+                //回调
+                if (CallBacks.ContainsKey(fileName))
+                {
+                    Action<string> action = CallBacks[fileName];
+                    if (action != null)
+                    {
+                       Delegate[] actions = action.GetInvocationList();
+                        foreach (var dele in actions)
+                        {
+                            var act = (Action<string>)dele;
+                            try
+                            {
+                                act(content);
+                            }
+                            catch (Exception e)
+                            {
+                                UnityEngine.Debug.LogError("异步加载config文件出错 "+fileName);
+                            }
+                        }
+                    }
+                    CallBacks.Remove(fileName);
+                }
+                yield return null;
+            }
+        }
     }
 }
